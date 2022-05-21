68c98272e46d8f219d44ec95f644963e05ad9e73
Anderson0312 <andersonmoura812@gmail.com>
2022-05-21 19:33:37 -0300

    att

--- js/script.js
+++ js/script.js
@@ -6,3 +6,54 @@ var typed = new Typed('.typing', {
     BackSpeed:60,
     loop:true
 })
+
+
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length,
+    allsection = document.querySelectorAll('.section'),
+    totalSelection = allsection.length;
+
+    for (let i = 0; i < totalNavList; i++) {
+        const a = navList[i].querySelector('a');
+        a.addEventListener('click', function() 
+        {
+            for(let i = 0; i < totalSelection; i++) {
+                allsection[i].classList.remove('back-section');
+            }
+            for(let j = 0; j < totalNavList; j++) {
+                if(navList[j].querySelector('a').classList.contains('active'))
+                {
+                    allsection[j].classList.add('back-section');
+                }
+                navList[j].querySelector('a').classList.remove('active');
+            }
+            this.classList.add('active');
+            showSection(this);
+            if(window.innerWidth < 1200) {
+                asideSectionTogglerBtn();
+            }
+        })
+    }
+
+    function showSection(element){
+        for(let i = 0; i < totalSelection; i++) {
+            allsection[i].classList.remove('active');
+        }
+        const target = element.getAttribute('href').split('#')[1];
+        document.querySelector('#' + target).classList.add('active');
+    }
+
+    const navTogglerBtn = document.querySelector('.nav-toggler'),
+        aside = document.querySelector('.aside');
+        navTogglerBtn.addEventListener('click',() => {
+            asideTogglerBtn();
+        })
+
+        function asideTogglerBtn() {
+            aside.classList.toggle('open');
+            navTogglerBtn.classList.toggle('open');
+            for(let i = 0; i < totalSelection; i++) {
+                allsection[i].classList.toggle('open');
+            }
+        }
