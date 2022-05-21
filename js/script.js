85a3d4bdf8e18071b354fd6e1ca5994edc5099cc
Anderson0312 <andersonmoura812@gmail.com>
2022-05-21 17:37:24 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typed animations
+
+var typed = new Typed('.typing', {
+    strings: ['','Desenvolvedor back/end', 'Desenvolvedor front/end'],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
